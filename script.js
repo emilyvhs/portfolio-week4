d2ba56ba42b4b4053ccd128b4a8865282abb9925
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-01-27 16:10:25 +0000

    Fetched project details from JSON file

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const portfolio = document.querySelector('#portfolio')
+
+fetch ('./projects.json')
+    .then ((response) => {
+        return response.json()
+    })
+
+    .then (data => {
+        
+        data.projects.forEach((project) => {
+            
+            let portfolioBox = document.querySelector('.portfolio-box')
+            portfolioBox.innerHTML += `
+                <h3>${project.title}</h3>
+                <img src="${project.image}" alt="${project.alt}" />
+                <p>${project.description}</p>
+                <a href=${project.link}>github link</a>
+            `        
+    })
+})      
+   
