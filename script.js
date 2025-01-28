2cfda8ad1e14225b0bb2aa1fe7f5ff8776a96a06
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-01-28 16:05:25 +0000

    Styled portfolio boxes

--- script.js
+++ script.js
@@ -2,26 +2,26 @@
 
 const portfolio = document.querySelector('#portfolio')
 
-// fetch ('./projects.json')
-//     .then ((response) => {
-//         return response.json()
-//     })
+fetch ('./projects.json')
+    .then ((response) => {
+        return response.json()
+    })
 
-//     .then (data => {
+    .then (data => {
         
-//         data.projects.forEach((project) => {
+        data.projects.forEach((project) => {
             
-//             let portfolioBox = document.querySelector('.portfolio')
-//             portfolioBox.innerHTML += `
-//                 <div class="portfolio-box">
-//                     <h3>${project.title}</h3>
-//                     <img src="${project.image}" alt="${project.alt}" />
-//                     <p>${project.description}</p>
-//                     <a href=${project.link}>github link</a>
-//                 </div>
-//             `        
-//     })
-// })      
+            let portfolioBox = document.querySelector('.portfolio')
+            portfolioBox.innerHTML += `
+                <div class="portfolio-box">
+                    <h3>${project.title}</h3>
+                    <img src="${project.image}" alt="${project.alt}" />
+                    <p>${project.description}</p>
+                    <a href=${project.link} target="_blank">github link</a>
+                </div>
+            `        
+    })
+})      
 
 let mobileNav = document.querySelector('.top-nav')
 let mobileContactLinks = document.querySelector('.header-contact-links')
